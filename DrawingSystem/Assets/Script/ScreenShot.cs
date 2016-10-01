aa909be19b41e2319e159b55079cce92d3e6cbbc
GEORGE QUENTIN <ggeoo93@gmail.com>
2016-10-01 13:50:20 +0100

    drawing and creating texture

---
+++ DrawingSystem/Assets/Script/ScreenShot.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour {
+
+	private Texture2D screenCap;
+	private Texture2D border;
+
+	private Rect captureFrame;
+
+	private Vector2 top;
+	private Vector2 bottom;
+	private Vector2 left;
+	private Vector2 right;
+
+	private int strokeLength = 2;
+	private int width = 300;
+	private int height = 200;
+
+	private bool shot = false;
+
+	public enum CaptureType { border, screen };
+	public CaptureType captureType = CaptureType.border;
+
+	[Range(10f,50f)] public float borderSpeed = 10f;
+
+	void setBorder(){
+
+
+		switch (captureType) {
+
+		case CaptureType.border:  
+			screenCap = new Texture2D( width, height, TextureFormat.RGB24, false);  
+
+			border = new Texture2D(2, 2, TextureFormat.ARGB32, false);
+			border.name = "border";
+			border.Apply ();
+
+			top = new Vector2 (200, 100);
+			bottom = new Vector2 (200, 300);
+			left = new Vector2 (200, 100);
+			right = new Vector2 (500, 100);
+
+			break;	
+		case CaptureType.screen: 
+			screenCap = new Texture2D( Screen.width, Screen.height, TextureFormat.RGB24, false);
+			break;
+		}
+
+
+
+	}
+
+
+	void OnGUI(){
+
+		switch (captureType) {
+
+		case CaptureType.border:  
+			
+
+			Vector2 move = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+
+
+			top += move * borderSpeed * Time.deltaTime;
+			bottom += move * borderSpeed * Time.deltaTime;
+			left += move * borderSpeed * Time.deltaTime;
+			right += move * borderSpeed * Time.deltaTime;
+
+
+			GUI.DrawTexture (new Rect (top, new Vector2 (width, strokeLength)), border, ScaleMode.StretchToFill); //top
+			GUI.DrawTexture (new Rect (bottom, new Vector2 (width, strokeLength)), border, ScaleMode.StretchToFill); //bottom
+			GUI.DrawTexture (new Rect (left, new Vector2 (strokeLength, height)), border, ScaleMode.StretchToFill); //left
+			GUI.DrawTexture (new Rect (right, new Vector2 (strokeLength, height)), border, ScaleMode.StretchToFill); //right
+
+			break;
+		}
+
+		//GUI.DrawTexture(new Rect( 0, 0, 1267, 5), border, ScaleMode.StretchToFill); //check screen width
+		//GUI.DrawTexture(new Rect( 0, 0, 5, 663), border, ScaleMode.StretchToFill); //check screen height
+
+		if (shot) {
+
+			GUI.DrawTexture(new Rect( 10, 10, 100, 100), screenCap, ScaleMode.StretchToFill); //result
+		}
+	}
+
+
+	IEnumerator capture(){
+
+		yield return new WaitForEndOfFrame ();
+
+		switch (captureType) {
+
+		case CaptureType.border:  
+			captureFrame = new Rect (bottom.x +1, bottom.y+ 62, width, height  ); 
+			break;
+		case CaptureType.screen: 
+			captureFrame = new Rect (0, 0, Screen.width, Screen.height);
+			break;
+		}
+
+		screenCap.ReadPixels(captureFrame,0,0,true);
+		screenCap.Apply();
+
+		shot = true;
+
+		GameObject.Find ("Cube").GetComponent<MeshRenderer> ().material.mainTexture = screenCap;
+		GameObject.Find ("Plane").GetComponent<MeshRenderer> ().material.mainTexture = screenCap;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+		setBorder ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			StartCoroutine ("capture");
+
+		}
+
+	}
+
+
+}
---
+++ DrawingSystem/Assets/Script/TestLineRenderer.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLineRenderer : MonoBehaviour {
+
+
+	public GameObject lineDrawPrefabs = null; // this is where we put the prefabs object
+
+	private bool isMousePressed;
+	private GameObject lineDrawPrefab;
+	private LineRenderer lineRenderer;
+	private List<Vector3> drawPoints = new List<Vector3>();
+	private Gradient g = new Gradient ();
+
+
+	// Use this for initialization
+	void Start () {
+		isMousePressed = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButtonDown(1))
+		{
+			// delete the LineRenderers when right mouse down
+			GameObject [] delete = GameObject.FindGameObjectsWithTag("LineDraw");
+			int deleteCount = delete.Length;
+			for(int i = deleteCount - 1; i >= 0; i--)
+				Destroy(delete[i]);
+		}
+
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			// left mouse down, make a new line renderer
+			isMousePressed = true;
+			setUpLine ("line",0.2f);
+
+		}
+		else if(Input.GetMouseButtonUp(0))
+		{
+			// left mouse up, stop drawing
+			isMousePressed = false;
+		}
+
+		if(isMousePressed)
+		{
+
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			//print (ray.origin);
+
+			Vector3 mousePos = ray.origin;
+			//print (mousePos);
+
+			if (!drawPoints.Contains (mousePos)) 
+			{
+				drawPoints.Add (mousePos);
+				DrawLine (drawPoints,mousePos);
+			}
+		}
+		if (!isMousePressed) {
+			drawPoints.Clear ();
+		}
+
+
+
+
+	}
+
+	void setUpLine(string lineName,float width){
+
+		lineDrawPrefab = GameObject.Instantiate(lineDrawPrefabs) as GameObject;
+		lineDrawPrefab.name = lineName;
+		lineRenderer = lineDrawPrefab.GetComponent<LineRenderer>();
+		lineRenderer.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+
+		lineRenderer.SetColors (Color.black,ExtensionMethods.RandomColor());
+		lineRenderer.SetWidth(width, width);
+		lineRenderer.SetVertexCount(0);
+
+
+	}
+	void DrawLine(List<Vector3> drawPoints, Vector3 mousePosition)
+	{
+		
+		lineRenderer.SetVertexCount (drawPoints.Count);
+		lineRenderer.SetPosition(drawPoints.Count - 1, mousePosition);
+
+	}
+
+//	Color colors(Color color1, Color color2, Color color3, Color color4, float time){
+//
+//
+//		Color[] colors = new Color[]{};
+//		GradientColorKey[] gradientColorKey = new GradientColorKey[] {
+//			new GradientColorKey (color1, 0f), 
+//			new GradientColorKey (color2, 0.35f), 
+//			new GradientColorKey (color3, 0.7f),
+//			new GradientColorKey (color4, 1f)
+//		};
+//		GradientAlphaKey[] gradientAlphaKey = new GradientAlphaKey[] {
+//
+//			new GradientAlphaKey (1, 0f),
+//			new GradientAlphaKey (1, 0.35f),
+//			new GradientAlphaKey (1, 0.7f),
+//			new GradientAlphaKey (1, 1)
+//		};
+//
+//		g.SetKeys(gradientColorKey, gradientAlphaKey);
+//
+//		return Color32.Lerp(g.Evaluate(0), g.Evaluate(1), time);
+//
+//	}
+
+
+}
